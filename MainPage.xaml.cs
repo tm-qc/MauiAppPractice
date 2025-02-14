7053dc731c59dddb10c270ec0d2d5811db59fdfc
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2025-02-14 15:51:19 +0900

    メニューの黒板設定のブロックに黒板一覧、黒板編集画面への遷移機能を追加
    Button化
    関数追加、紐づけ
    
    他のブロックも遷移機能つけるときは同じようにするとOK

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,4 +6,12 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    //黒板一覧・編集画面への遷移
+    private async void OnGoToBlackboardListPage(object sender, EventArgs e)
+    {
+        //ルート名で指定するが・・
+        //ShellContent(MauiAppPractice\AppShell.xaml)に登録されたページでトップレベルのページ (ShellContent) にナビゲーションするときは絶対パス /// で指定する必要がある
+        await Shell.Current.GoToAsync("///BlackboardListPage");
+    }
 }
