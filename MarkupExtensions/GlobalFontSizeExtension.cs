f3d3e6ed1e4f32c405788c669a03ce2bfad7fff6
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2025-02-17 16:37:06 +0900

    XAML マークアップ拡張の実装：XAMLコントロールのプロパティの値の共通化
    -----------
    XAMLコントロールのプロパティの値をハードコーディングじゃなく
    共通の定義からそれぞれのコントロールにビルドのときにあてこむ方法
    
    同じ値なのに定義がいろんなとこにあるとメンテナンスできなくなる
    
    作成方法
     例）共通のフォントサイズの指定
    
     マークアップ拡張クラスを作成、クラスファイルを置くなければフォルダも作成
      MauiAppPractice\MarkupExtensions\GlobalFontSizeExtension.cs
     xamlに名前空間読込追加
      MauiAppPractice\MainPage.xaml
    
     xamlで設定読みこみ
      MauiAppPractice\MainPage.xaml
      FontSize="{me:GlobalFontSizeExtension MainPage.MenuFont}"

---
+++ MarkupExtensions/GlobalFontSizeExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppPractice.MarkupExtensions
+{
+    //[ContentProperty(nameof(SizeKey))] は SizeKey をデフォルトプロパティにするための属性
+    //デフォルトプロパティとは？xamlで参照する際にSizeKey = を省略して XAML を簡潔に書ける
+    //nameof(SizeKey) を使うのは型安全性や修正漏れのときにエラーが出る状態を確保し、リファクタリング時のミスを防ぐため
+    //SizeKey の名前は自由に決めてOK
+    [ContentProperty(nameof(SizeKey))]
+    public class GlobalFontSizeExtension : IMarkupExtension
+    {
+        public string SizeKey { get; set; } = "Default";
+
+        //ProvideValueの名前はIMarkupExtension インターフェースで定義されているメソッド名で変更不可
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            //C#でのSwitch文：SizeKeyという名前で受け取った名前と一致した値を返す
+            return SizeKey switch
+            {
+                "MainPage.Title" => 24.0,
+                "MainPage.MenuFont" => 18.0,
+                _ => 16.0, // デフォルトフォントサイズ
+            };
+        }
+    }
+
+}
